f120d750dc5b5c9e50345f84d110afc9f8e8b6c2
Shaquille Rivera <shaq.rivera@gmail.com>
2022-04-01 02:02:31 -0400

    added typedefs

---
+++ 02-Homework/Develop/server/schemas/resolvers.js
@@ -0,0 +1,25 @@
+const { Book, User } = require('../models');
+
+const resolvers = {
+  Query: {
+    getSingleUser: async (parent, args) => {
+      return User.findById(args._id)
+    },
+  },
+  Mutation: {
+    createMatchup: async (parent, args) => {
+      const matchup = await Matchup.create(args);
+      return matchup;
+    },
+    createVote: async (parent, { _id, techNum }) => {
+      const vote = await Matchup.findOneAndUpdate(
+        { _id },
+        { $inc: { [`tech${techNum}_votes`]: 1 } },
+        { new: true }
+      );
+      return vote;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ 02-Homework/Develop/server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Query {
+    getSingleUser(_id: String): User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): User
+    login(email: String, username: String, password: String!): User
+    saveBook(_id: ID!, savedBooks: [Book]): User
+    deleteBook(_id: ID!, savedBooks: [Book]): User
+  }
+`;
+
+module.exports = typeDefs;
